06f5e940124768823ba641699f77a0e819fb681c
Gerez Jimenez Juan Jose Armando <jjagerezjimenez@gmail.com>
2023-05-14 20:25:10 -0300

    correcciones

--- inc/digital.h
+++ inc/digital.h
@@ -1,37 +1,44 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifndef INSTANCIAS_GPIO
+#define INSTANCIAS_GPIO 4
+#endif
+
 #ifndef DIGITAL_H
 #define DIGITAL_H             //Buscar referencia del digital de los videos (hueco clases 11 y 12)
 
-#include <stdint.h>
-
-typedef struct DigitalInput *  DigitalInput_t;
-typedef struct DigitalOutput * DigitalOutput_t;
 
 
-DigitalInput_t DigitalInput_Create(uint8_t port, uint8_t pin, bool inverted);  //eror uint_8
+typedef struct DigitalInput_s *DigitalInput_t;
+typedef struct DigitalOutput_s *DigitalOutput_t;
 
 
-bool DigitalInput_GetState(DigitalInput_t input);
+DigitalInput_t DigitalInput_Create(uint8_t port,uint8_t inverted , uint8_t pin);  //eror uint_8
 
 
-bool DigitalInput_HasChange(DigitalInput_t input);
+bool DigitalInput_GetState(DigitalInput_t Input);
 
 
-bool DigitalInput_HasActivate(DigitalInput_t input);
+bool DigitalInput_HasChange(DigitalInput_t Input);
 
 
-bool DigitalInput_HasDesactivate(DigitalInput_t input);
+bool DigitalInput_HasActivate(DigitalInput_t Input);
+
+
+bool DigitalInput_HasDesactivate(DigitalInput_t Input);
 
 
 DigitalOutput_t DigitalOutput_Create(uint8_t port, uint8_t pin);
 
 
-void DigitalOutput_Activate(DigitalOutput_t input);
+void DigitalOutput_Activate(DigitalOutput_t Output);
 
 
-void DigitalOutput_Desactivate(DigitalOutput_t input);
+void DigitalOutput_Desactivate(DigitalOutput_t Output);
 
 
-void DigitalOutput_Toggle(DigitalOutput_t input);
+void DigitalOutput_Toggle(DigitalOutput_t Output);
 
 
 #endif
