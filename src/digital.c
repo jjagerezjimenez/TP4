860496a60242f3150dabe943c044fa723281fb8e
Gerez Jimenez Juan Jose Armando <jjagerezjimenez@gmail.com>
2023-05-14 20:12:38 -0300

    Cambios en digital

--- src/digital.c
+++ src/digital.c
@@ -2,96 +2,88 @@
 #include <stdio.h>			// controlar
 #include <stdint.h>
 #include "digital.h"
+#include "chip.h"           //consultar
 
-#define INSTANCES 4		//ver 4
+//#define INSTANCIAS_GPIO 4		//ver 4
 
-struct DigitalInput {
+typedef struct DigitalInput_s {
     uint8_t port;
     uint8_t pin;
-    bool    inverted;
+    uint8_t inverted;       //Bool?
     bool    last_state;
     bool    ocupado;
-};
+}DigitalInput_t;
 
-struct DigitalOutput {
+typedef struct DigitalOutput_s {
     uint8_t port;
     uint8_t pin;
     bool    ocupado;
-};
+}DigitalOutput_t;
 
 
-static struct DigitalInput  Input[INSTANCES]  = {0};
-static struct DigitalOutput Output[INSTANCES] = {0};
+//static struct DigitalInput  Input[INSTANCES]  = {0};
+//static struct DigitalOutput Output[INSTANCES] = {0};
 
 
-DigitalInput_t DigitalInput_Create(uint8_t port, uint8_t pin, bool inverted) {
-    uint8_t posicion = 0;
 
-    for (int i = 0; (i < INSTANCES) & (posicion == 0); i++) {
-
-        if (Input[i].ocupado == false)
-            posicion = i;
+DigitalInput_t DigitalInput_Ocupado(void){
+    DigitalInput_t Input = NULL;
+    static struct DigitalInput_s instances[INSTANCIAS_GPIO]={0};
+    for(int i=0;i<INSTANCIAS_GPIO;i++){
+        if(!instances[i].ocupado){
+            instances[i].ocupado=true;
+            Input=&instances[i];
+            break;
+        }
     }
-    DigitalInput_t input = &Input[posicion];
-    input->port           = port;
-    input->pin            = pin;
-    input->inverted       = inverted;
-    input->ocupado        = true;
+    return Input;
+}
 
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, port, pin, false);
+
+DigitalInput_t DigitalInput_Create(uint8_t port, uint8_t inverted, uint8_t pin) {
+
+    DigitalInput_t input = DigitalInput_Ocupado();
+    if(Input){
+        Input->port           = port;
+        Input->pin            = pin;
+        Input->inverted       = inverted;
+
+        Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, Input->port, Input->pin, false);
+    }
+    
 
     return input;
 }
 
 
-bool DigitalInput_GetState(DigitalInput_t input) {
-
-    if (input->inverted == 0)
-
-        return Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, input->port, input->pin);
-    else
-        return !(Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, input->port, input->pin));		//el profesor formulo esto distinto, controlar
-}
+bool DigitalInput_GetState(DigitalInput_t Input) {
+    return  Input->inverted ^ Chip_GPIO_ReadPortBit(LPC_GPIO_PORT, Input->port, Input->pin);
+    }
 
 
-bool DigitalInput_HasChange(DigitalInput_t input) {
+bool DigitalInput_HasChange(DigitalInput_t Input) {
 
-    bool current_state = DigitalInput_GetState(input);
-    bool resultado;
-
-    if (current_state != input->last_state)
-        resultado = true;
-    else
-        resultado = false;
-    input->last_state = current_state;
+    bool current_state = DigitalInput_GetState(Input);
+    bool resultado = current_state != Input->last_state;
+    Input->last_state=current_state;
     return resultado;
 }
 
 
-bool DigitalInput_HasActivate(DigitalInput_t input) {
+bool DigitalInput_HasActivate(DigitalInput_t Input) {
 
-    bool current_state = DigitalInput_GetState(input);
-    bool resultado;
-
-    if ((0 == input->last_state) & (1 == current_state))
-        resultado = true;
-    else
-        resultado = false;
-    input->last_state = current_state;
+    bool current_state = DigitalInput_GetState(Input);
+    bool resultado = current_state && !Input->last_state;
+    Input->last_state= current_state;
     return resultado;
 }
 
 
-bool DigitalInput_HasDesactivate(DigitalInput_t input) {
+bool DigitalInput_HasDesactivate(DigitalInput_t Input) {
 
-    bool current_state = DigitalInput_GetState(input);
-    bool resultado;
-
-    if ((1 == input->last_state) & (0 == current_state))
-        resultado = true;
-    else
-        resultado = false;
-    input->last_state = current_state;
+    bool current_state = DigitalInput_GetState(Input);
+    bool resultado = !current_state && !Input->last_state;
+    Input->last_state= current_state;
     return resultado;
 }
 
@@ -99,39 +91,48 @@ bool DigitalInput_HasDesactivate(DigitalInput_t input) {
 
 //Salidas // Ver
 
+DigitalOutput_t DigitalOutput_Ocupado(void);
 
-DigitalOutput_t DigitalOutput_Create(uint8_t port, uint8_t pin) {
-
-    uint8_t posicion = 0;
-
-    for (int i = 0; (i < INSTANCES) & (posicion == 0); i++) {
-
-        if (Output[i].ocupado == false)
-            posicion = i;
+DigitalOutput_t DigitalOutput_Ocupado(void){
+    DigitalOutput_t output = NULL;
+    static struct DigitalOutput_s instances[INSTANCIAS_GPIO]={0};
+    for(int i=0;i<INSTANCIAS_GPIO;i++){
+        if(!instances[i].ocupado){
+            instances[i].ocupado=true;
+            output=&instances[i];
+            break;
+        }
     }
-
-    DigitalOutput_t output = &Output[posicion];
-    output->port            = port;
-    output->pin             = pin;
-    output->ocupado         = true;
-
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, port, pin, false);
-    Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, port, pin, true);
-
     return output;
 }
 
 
-void DigitalOutput_Activate(DigitalOutput_t output) {		//output?
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, output->port, output->pin, true);
+DigitalOutput_t DigitalOutput_Create(uint8_t port, uint8_t pin) {
+
+    DigitalOutput_t Output = DigitalOutput_Ocupado();
+    if(Output){
+        Output->port           = port;
+        Output->pin            = pin;
+
+        Chip_GPIO_SetPinDIR(LPC_GPIO_PORT, Output->port, Output->pin, false);
+        Chip_GPIO_SetPinState(LPC_GPIO_PORT, Output->port, Output->pin, false);
+    }
+    
+
+    return Output;
 }
 
 
-void DigitalOutput_Desactivate(DigitalOutput_t output) {
-    Chip_GPIO_SetPinState(LPC_GPIO_PORT, output->port, output->pin, false);
+void DigitalOutput_Activate(DigitalOutput_t Output) {		//output?
+    Chip_GPIO_SetPinState(LPC_GPIO_PORT, Output->port, Output->pin, true);
 }
 
 
-void DigitalOutput_Toggle(DigitalOutput_t output) {
-    Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, output->port, output->pin);
+void DigitalOutput_Desactivate(DigitalOutput_t Output) {
+    Chip_GPIO_SetPinState(LPC_GPIO_PORT, Output->port, Output->pin, false);
+}
+
+
+void DigitalOutput_Toggle(DigitalOutput_t Output) {
+    Chip_GPIO_SetPinToggle(LPC_GPIO_PORT, Output->port, Output->pin);
 }
